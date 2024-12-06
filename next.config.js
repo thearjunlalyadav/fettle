8e8500975a23c216896f6d1dc47c5cba56fadb60
arjun lal yadav <yadavarjunlal6@gamil.com>
2024-12-06 13:58:56 +0530

    first commit

--- next.config.js
+++ next.config.js
@@ -2,7 +2,4 @@ module.exports = {
   reactStrictMode: true,
   swcMinify: true,
   assetPrefix: process.env.NODE_ENV === 'production' ? '/' : '',
-  images: {
-    unoptimized: true, // Disable image optimization
-  },
 }
