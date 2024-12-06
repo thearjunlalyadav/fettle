60987dcb4439e5f98db3da32214eaa781a2956b5
Sohan Yadav <sohanyadavdevops@gmail.com>
2024-12-06 10:49:36 +0530

    first commit

--- out/_next/static/08l9SV4w_g5go8yeLwqtX/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/422-b5ab615314c0b233.js","static/chunks/pages/index-5a7466c94e874f02.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
