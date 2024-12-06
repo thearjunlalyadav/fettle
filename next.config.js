ece4f364a184c9b185fee034014ea79e99448544
arjun lal yadav <yadavarjunlal6@gamil.com>
2024-12-06 13:45:16 +0530

    first commit

--- next.config.js
+++ next.config.js
@@ -3,6 +3,6 @@ module.exports = {
   swcMinify: true,
   assetPrefix: process.env.NODE_ENV === 'production' ? '/' : '',
   images: {
-    disableStaticImages: true, // This will disable image optimization during static export
+    unoptimized: true, // Disable image optimization
   },
 }
