940bd72ba09fe9277dc1d28fd37ef1399bb42332
arjun lal yadav <yadavarjunlal6@gamil.com>
2024-12-06 14:06:04 +0530

    first commit

--- next.config.js
+++ next.config.js
@@ -3,8 +3,6 @@ module.exports = {
   swcMinify: true,
   assetPrefix: process.env.NODE_ENV === 'production' ? '/' : '',
   images: {
-    disableStaticImages: true,  // Disable static image imports
-    loader: 'imgix',            // Use an external image loader, which works with `next export`
-    path: '',                   // Prevent the default Next.js image loader
+    unoptimized: true, // Disable Next.js Image Optimization API
   },
 }
